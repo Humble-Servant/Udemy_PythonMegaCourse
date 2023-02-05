7a459081385813a23a65a3da8c9b2cb2406a074b
We the People <69933074+Humble-Servant@users.noreply.github.com>
2023-02-05 15:01:35 -0500

    Implement "Add new task" feature

--- App1/web.py
+++ App1/web.py
@@ -3,9 +3,18 @@ from modules import functions
 
 tasks = functions.get_tasks()
 
+
+def add_task():
+    task = st.session_state["new_task"] + "\n"
+    tasks.append(task)
+    functions.write_tasks(tasks)
+
+
 st.title("My Task List")
 
 for task in tasks:
     st.checkbox(task)
 
-st.text_input(label="", placeholder="Enter a task...")
+st.text_input(label=" ", placeholder="Enter a task...", on_change=add_task, key="new_task")
+
+st.session_state
